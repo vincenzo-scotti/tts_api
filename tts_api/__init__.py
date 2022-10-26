ee0c4c2a01bb0fe75417403d48823f11f5c78ee4
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-10-26 18:22:51 +0200

    Refactor api package path.

--- tts_api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .api import load_tts, load_vocoder, load_speaker_encoder
-from .api import synthesise_speech
-from .api import get_speaker_embedding, get_prosody_embedding, get_gst, get_gst_embedding_src_tgt
